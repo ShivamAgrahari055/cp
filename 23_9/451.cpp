1aa3ba07fff5f5f3ae054aeb6457f9ea28139488
ShivamAgrahari055 <agrahari8840@gmail.com>
2026-09-23 21:51:25 +0530

    leetcode 451

---
+++ 23_9/451.cpp
@@ -0,0 +1,39 @@
+
+
+class Compare {
+public:
+    bool operator()(pair<char, int> a, pair<char, int> b) {
+        return a.second < b.second;
+    }
+};
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        priority_queue<pair<char, int>, vector<pair<char, int>>, Compare> pq;
+
+        map<char, int> m;
+
+        for (auto &x : s) {
+            m[x]++;
+        }
+
+        for (auto &[u, v] : m) {
+            pq.push({u, v});
+        }
+
+        string ans = "";
+
+        while (!pq.empty()) {
+            char ch = pq.top().first;
+            int freq = pq.top().second;
+            pq.pop();
+
+            while (freq--) {
+                ans += ch;
+            }
+        }
+
+        return ans;
+    }
+};
